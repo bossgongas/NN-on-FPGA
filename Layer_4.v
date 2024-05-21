0e133104693062459a38dadb3c6a12824bd51035
bossgongas <76745186+bossgongas@users.noreply.github.com>
2024-05-21 11:30:47 +0100

    Create the Layers
    4 layers - 30, 30, 10, 2, input is 784(28*28)

---
+++ Layer_4.v
@@ -0,0 +1,44 @@
+module Layer_4 #(parameter NN = 30,numWeight=784,dataWidth=16,layerNum=1,sigmoidSize=10,weightIntWidth=4,actType="sigmoid")
+    (
+    input           clk,
+    input           rst,
+    input           weightValid,
+    input           biasValid,
+    input [31:0]    weightValue,
+    input [31:0]    biasValue,
+    input [31:0]    config_layer_num,
+    input [31:0]    config_neuron_num,
+    input           x_valid,
+    input [dataWidth-1:0]    x_in,
+    output [NN-1:0]     o_valid,
+    output [NN*dataWidth-1:0]  x_out
+    );
+neuron #(.numWeight(numWeight),.layerNo(layerNum),.neuronNo(0),.dataWidth(dataWidth),.sigmoidSize(sigmoidSize),.weightIntWidth(weightIntWidth),.actType(actType),.weightFile("w_4_0.mif"),.biasFile("b_4_0.mif"))n_0(
+        .clk(clk),
+        .rst(rst),
+        .myinput(x_in),
+        .weightValid(weightValid),
+        .biasValid(biasValid),
+        .weightValue(weightValue),
+        .biasValue(biasValue),
+        .config_layer_num(config_layer_num),
+        .config_neuron_num(config_neuron_num),
+        .myinputValid(x_valid),
+        .out(x_out[0*dataWidth+:dataWidth]),
+        .outvalid(o_valid[0])
+        );
+neuron #(.numWeight(numWeight),.layerNo(layerNum),.neuronNo(1),.dataWidth(dataWidth),.sigmoidSize(sigmoidSize),.weightIntWidth(weightIntWidth),.actType(actType),.weightFile("w_4_1.mif"),.biasFile("b_4_1.mif"))n_1(
+        .clk(clk),
+        .rst(rst),
+        .myinput(x_in),
+        .weightValid(weightValid),
+        .biasValid(biasValid),
+        .weightValue(weightValue),
+        .biasValue(biasValue),
+        .config_layer_num(config_layer_num),
+        .config_neuron_num(config_neuron_num),
+        .myinputValid(x_valid),
+        .out(x_out[1*dataWidth+:dataWidth]),
+        .outvalid(o_valid[1])
+        );
+endmodule
