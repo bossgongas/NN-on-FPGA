0c0d185ec1227ec1aca6ad9931403308a84ec2be
bossgongas <76745186+bossgongas@users.noreply.github.com>
2024-05-22 11:11:06 +0100

    Update neuron.v
    Quartus does not support $readmemb input string to be a variable, it must be a literal string.

--- neuron.v
+++ neuron.v
@@ -68,7 +68,88 @@ module neuron #(parameter layerNo=0,neuronNo=0,numWeight=784,dataWidth=16,sigmoi
     `ifdef pretrained
         initial
         begin
-            $readmemb(biasFile,biasReg);
+            //$readmemb(biasFile,biasReg); não funciona desta forma
+            case (layerNo)
+				0: case (neuronNo) // Layer 0 with 30 neurons
+						0: $readmemb(`BIAS_FILE_0_0, biasReg);
+						1: $readmemb(`BIAS_FILE_0_1, biasReg);
+						2: $readmemb(`BIAS_FILE_0_2, biasReg);
+						3: $readmemb(`BIAS_FILE_0_3, biasReg);
+						4: $readmemb(`BIAS_FILE_0_4, biasReg);
+						5: $readmemb(`BIAS_FILE_0_5, biasReg);
+						6: $readmemb(`BIAS_FILE_0_6, biasReg);
+						7: $readmemb(`BIAS_FILE_0_7, biasReg);
+						8: $readmemb(`BIAS_FILE_0_8, biasReg);
+						9: $readmemb(`BIAS_FILE_0_9, biasReg);
+						10: $readmemb(`BIAS_FILE_0_10, biasReg);
+						11: $readmemb(`BIAS_FILE_0_11, biasReg);
+						12: $readmemb(`BIAS_FILE_0_12, biasReg);
+						13: $readmemb(`BIAS_FILE_0_13, biasReg);
+						14: $readmemb(`BIAS_FILE_0_14, biasReg);
+						15: $readmemb(`BIAS_FILE_0_15, biasReg);
+						16: $readmemb(`BIAS_FILE_0_16, biasReg);
+						17: $readmemb(`BIAS_FILE_0_17, biasReg);
+						18: $readmemb(`BIAS_FILE_0_18, biasReg);
+						19: $readmemb(`BIAS_FILE_0_19, biasReg);
+						20: $readmemb(`BIAS_FILE_0_20, biasReg);
+						21: $readmemb(`BIAS_FILE_0_21, biasReg);
+						22: $readmemb(`BIAS_FILE_0_22, biasReg);
+						23: $readmemb(`BIAS_FILE_0_23, biasReg);
+						24: $readmemb(`BIAS_FILE_0_24, biasReg);
+						25: $readmemb(`BIAS_FILE_0_25, biasReg);
+						26: $readmemb(`BIAS_FILE_0_26, biasReg);
+						27: $readmemb(`BIAS_FILE_0_27, biasReg);
+						28: $readmemb(`BIAS_FILE_0_28, biasReg);
+						29: $readmemb(`BIAS_FILE_0_29, biasReg);
+				   endcase
+				1: case (neuronNo) // Layer 1 with 30 neurons
+						0: $readmemb(`BIAS_FILE_1_0, biasReg);
+						1: $readmemb(`BIAS_FILE_1_1, biasReg);
+						2: $readmemb(`BIAS_FILE_1_2, biasReg);
+						3: $readmemb(`BIAS_FILE_1_3, biasReg);
+						4: $readmemb(`BIAS_FILE_1_4, biasReg);
+						5: $readmemb(`BIAS_FILE_1_5, biasReg);
+						6: $readmemb(`BIAS_FILE_1_6, biasReg);
+						7: $readmemb(`BIAS_FILE_1_7, biasReg);
+						8: $readmemb(`BIAS_FILE_1_8, biasReg);
+						9: $readmemb(`BIAS_FILE_1_9, biasReg);
+						10: $readmemb(`BIAS_FILE_1_10, biasReg);
+						11: $readmemb(`BIAS_FILE_1_11, biasReg);
+						12: $readmemb(`BIAS_FILE_1_12, biasReg);
+						13: $readmemb(`BIAS_FILE_1_13, biasReg);
+						14: $readmemb(`BIAS_FILE_1_14, biasReg);
+						15: $readmemb(`BIAS_FILE_1_15, biasReg);
+						16: $readmemb(`BIAS_FILE_1_16, biasReg);
+						17: $readmemb(`BIAS_FILE_1_17, biasReg);
+						18: $readmemb(`BIAS_FILE_1_18, biasReg);
+						19: $readmemb(`BIAS_FILE_1_19, biasReg);
+						20: $readmemb(`BIAS_FILE_1_20, biasReg);
+						21: $readmemb(`BIAS_FILE_1_21, biasReg);
+						22: $readmemb(`BIAS_FILE_1_22, biasReg);
+						23: $readmemb(`BIAS_FILE_1_23, biasReg);
+						24: $readmemb(`BIAS_FILE_1_24, biasReg);
+						25: $readmemb(`BIAS_FILE_1_25, biasReg);
+						26: $readmemb(`BIAS_FILE_1_26, biasReg);
+						27: $readmemb(`BIAS_FILE_1_27, biasReg);
+						28: $readmemb(`BIAS_FILE_1_28, biasReg);
+						29: $readmemb(`BIAS_FILE_1_29, biasReg);
+				   endcase
+				2: case (neuronNo) // Layer 2 with 10 neurons
+						0: $readmemb(`BIAS_FILE_2_0, biasReg);
+						1: $readmemb(`BIAS_FILE_2_1, biasReg);
+						2: $readmemb(`BIAS_FILE_2_2, biasReg);
+						3: $readmemb(`BIAS_FILE_2_3, biasReg);
+						4: $readmemb(`BIAS_FILE_2_4, biasReg);
+						5: $readmemb(`BIAS_FILE_2_5, biasReg);
+						6: $readmemb(`BIAS_FILE_2_6, biasReg);
+						7: $readmemb(`BIAS_FILE_2_7, biasReg);
+						8: $readmemb(`BIAS_FILE_2_8, biasReg);
+						9: $readmemb(`BIAS_FILE_2_9, biasReg);
+				   endcase
+				3: case (neuronNo) // Layer 3 with 1 neuron
+						0: $readmemb(`BIAS_FILE_3_0, biasReg);
+				   endcase
+			endcase
         end
         always @(posedge clk)
         begin
